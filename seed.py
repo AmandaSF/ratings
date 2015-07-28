5f9b20f67e9f82f914c9a944bb3a7bd35c889b2d
Hackbright Student <info@hackbrightacademy.com>
2015-07-27 18:00:24 -0700

    working on population

--- seed.py
+++ seed.py
@@ -10,11 +10,29 @@ def load_users():
 
 def load_movies():
     """Load movies from u.item into database."""
+    #dates are given as strings, we need to store date as actual date object
+    #research Python datetime library to find function parsing string into object
+
 
 
 def load_ratings():
     """Load ratings from u.data into database."""
 
+    input_text = open("u.data")
+
+    for strings in input_text:
+        input_text.split('\t')
+        user_id = input_text[0]
+        movie_id = input_text[1]
+        score = input_text[2]
+
+INSERT INTO ratings (NULL, )
+
+#we are currently trying to figure out how to use SQLAlchemy SYNTAX to insert our data
+#into the database. We know how to use SQLite3, so see if that helps
+#we have currently split our lines into indexes and we're ready to insert into tables
+#we need to figure out how primary autoincremting keys will work if we don't know the value
+#(EG, can we use null if it's not a null value)    
 
 if __name__ == "__main__":
     connect_to_db(app)
